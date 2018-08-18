f2c5e0775289781d85e8e90432d5b069b972257a
Mun Jun Ho <projectgaon@gmail.com>
2018-08-18 12:35:48 +0900

    html template  추가

--- src/main/java/com/gaonit/web/WelcomeController.java
+++ src/main/java/com/gaonit/web/WelcomeController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
 	@GetMapping("/helloworld")
-	public String welcome(String name, int age, Model model) {
-		System.out.println("name: " + name + " age: " + age);
-		model.addAttribute("name", name);
-		model.addAttribute("age", age);
+	public String welcome(Model model) {
+		model.addAttribute("name", "Chris");
+		model.addAttribute("value", 10000);
+		model.addAttribute("taxed_value",  10000 - (10000 * 0.4));
+		model.addAttribute("in_ca", false);
 		return "welcome";
 	}
 }
